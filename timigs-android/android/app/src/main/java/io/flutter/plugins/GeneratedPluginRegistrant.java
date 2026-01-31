abd8e55ad9528021a45cc4bbbc3e518d61fcc488
BANER <86590991+BANSAFAn@users.noreply.github.com>
2026-01-31 21:34:18 +0200

    CI: Update

--- timigs-android/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ timigs-android/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -41,16 +41,6 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin share_plus, dev.fluttercommunity.plus.share.SharePlusPlugin", e);
     }
     try {
-      flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
-    }
-    try {
-      flutterEngine.getPlugins().add(new com.tekartik.sqflite.SqflitePlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin sqflite_android, com.tekartik.sqflite.SqflitePlugin", e);
-    }
-    try {
       flutterEngine.getPlugins().add(new io.github.parassharmaa.usage_stats.UsageStatsPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin usage_stats, io.github.parassharmaa.usage_stats.UsageStatsPlugin", e);
